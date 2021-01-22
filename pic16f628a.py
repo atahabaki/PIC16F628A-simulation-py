638ac70155a900d47e8aead4b56c205320240c4e
A. Taha Baki <a.taha.baki@outlook.com>
2021-01-22 19:42:09 +0300

    calculating the cycle of command...

--- pic16f628a.py
+++ pic16f628a.py
@@ -38,6 +38,15 @@ class PIC16F628A:
             FileRegister(name="F35",bits=35),
             FileRegister(name="F36",bits=36)
         ]
+        self.KCS=0
+        self.skipBTFSS=None
+        self.skipDECFSZ=None
+
+    def __increase_KCS(self,inc=1):
+        self.KCS+=inc
+
+    def goto(self):
+        self.__increase_KCS(inc=2)
 
     def movf(self,f,d):
         """
@@ -51,6 +60,7 @@ class PIC16F628A:
             self.RAM[f].bits = self.RAM[f].bits
         elif d == 0:
             self.Accumulator.bits = self.RAM[f].bits
+        self.__increase_KCS()
         return self.RAM[f].bits
 
     def movwf(self,f):
@@ -61,6 +71,7 @@ class PIC16F628A:
             Index of a single RAM unit (a fileregister)
         """
         self.RAM[f].bits=self.Accumulator.bits
+        self.__increase_KCS()
         return self.Accumulator.bits
 
     def movlw(self,k):
@@ -71,6 +82,7 @@ class PIC16F628A:
             0-255
         """
         self.Accumulator.bits=k
+        self.__increase_KCS()
         return self.Accumulator.bits
 
     # Mathematical
@@ -97,9 +109,11 @@ class PIC16F628A:
                 self.StatusFileRegister.change_bit(index=2,bit=1)
             else:
                 self.StatusFileRegister.change_bit(index=2,bit=0)
+        self.__increase_KCS()
 
     def addlw(self,k):
         self.Accumulator.assign_bits(k + self.Accumulator.bits)
+        self.__increase_KCS()
 
     def subwf(self,f,d):
         if d == 1:
@@ -116,82 +130,99 @@ class PIC16F628A:
                 self.StatusFileRegister.change_bit(index=2,bit=1)
             else:
                 self.StatusFileRegister.change_bit(index=2,bit=0)
+        self.__increase_KCS()
 
     def sublw(self,k):
         self.Accumulator.assign_bits(k - self.Accumulator.bits)
+        self.__increase_KCS()
 
     def incf(self,f,d):
         if d == 1:
             self.RAM[f].assign_bits(self.RAM[f].bits + 1)
         elif d == 0:
             self.Accumulator.assign_bits(self.RAM[f].bits + 1)
+        self.__increase_KCS()
 
     def decf(self,f,d):
         if d == 1:
             self.RAM[f].assign_bits(self.RAM[f].bits-1)
         elif d == 0:
             self.Accumulator.assign_bits(self.RAM[f].bits-1)
+        self.__increase_KCS()
     
     # Boolean...
 
     def andlw(self,k):
         self.Accumulator.assign_bits(k & self.Accumulator.bits)
+        self.__increase_KCS()
 
     def andwf(self,f,d):
         if d == 1:
             self.RAM[f].assign_bits(self.RAM[f].bits & self.Accumulator.bits)
         elif d == 0:
             self.Accumulator.assign_bits(self.RAM[f].bits & self.Accumulator.bits)
+        self.__increase_KCS()
 
     def iorlw(self,k):
         self.Accumulator.assign_bits(k | self.Accumulator.bits)
+        self.__increase_KCS()
 
     def iorwf(self,f,d):
         if d == 1:
             self.RAM[f].assign_bits(self.RAM[f].bits | self.Accumulator.bits)
         elif d == 0:
             self.Accumulator.assign_bits(self.RAM[f].bits | self.Accumulator.bits)
+        self.__increase_KCS()
 
     def xorlw(self,k):
         self.Accumulator.assign_bits(k ^ self.Accumulator.bits)
+        self.__increase_KCS()
 
     def xorwf(self,f,d):
         if d == 1:
             self.RAM[f].assign_bits(self.RAM[f].bits ^ self.Accumulator.bits)
         elif d == 0:
             self.Accumulator.assign_bits(self.RAM[f].bits ^ self.Accumulator.bits)
+        self.__increase_KCS()
 
     def comf(self,f,d):
         if d == 1:
             self.RAM[f].assign_bits(self.RAM[f].ones_complement())
         elif d == 0:
             self.Accumulator.assign_bits(self.RAM[f].ones_complement())
+        self.__increase_KCS()
 
     # Misc and other stuff...
 
     def clrf(self,f):
         self.RAM[f].bits = 0
+        self.__increase_KCS()
 
     def clrw(self):
         self.Accumulator.bits = 0
+        self.__increase_KCS()
 
     def bcf(self,f,b):
         self.RAM[f].change_bit(index=b,bit=0)
+        self.__increase_KCS()
 
     def bsf(self,f,b):
         self.RAM[f].change_bit(index=b,bit=1)
+        self.__increase_KCS()
 
     def rrf(self,f,d):
         if d == 1:
             self.RAM[f].rotate_right()
         elif d == 0:
             self.Accumulator.bits = self.Accumulator.bits << 1
+        self.__increase_KCS()
 
     def rlf(self,f,d):
         if d == 1:
             self.RAM[f].rotate_right()
         elif d == 0:
             self.Accumulator.bits = self.Accumulator.bits >> 1
+        self.__increase_KCS()
 
     def swapf(self,f,d):
         pass
@@ -200,19 +231,36 @@ class PIC16F628A:
         pass
 
     def nop(self):
-        pass
+        self.__increase_KCS()
 
     def btfsc(self,f,b):
         pass
 
     def btfss(self,f,b):
         if self.RAM[f].get_bit(b) == 1:
-            self.skip = True
+            self.skipBTFSS = True
+            self.__increase_KCS(inc=2)
         else:
-            self.skip = False
+            self.skipBTFSS = False
+            self.__increase_KCS()
 
     def incfsz(self,f,d):
         pass
 
     def decfsz(self,f,d):
-        pass
+        if d == 1:
+            self.RAM[f].assign_bits(self.RAM[f].bits-1)
+            if self.RAM[f].bits == 0:
+                self.skipDECFSZ=True
+                self.__increase_KCS(inc=2)
+            else:
+                self.skipDECFSZ=False
+                self.__increase_KCS()
+        elif d == 0:
+            self.Accumulator.assign_bits(self.RAM[f].bits-1)
+            if self.Accumulator.bits == 0:
+                self.skipDECFSZ=True
+                self.__increase_KCS(inc=2)
+            else:
+                self.skipDECFSZ=False
+                self.__increase_KCS()
