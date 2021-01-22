0a5a27ae06897dbe8467c10b78099e22eff95ad6
A. Taha Baki <a.taha.baki@outlook.com>
2021-01-22 12:16:30 +0300

    written tests 4 all func...

--- test_fileregister.py
+++ test_fileregister.py
@@ -67,6 +67,10 @@ class TestFileRegister(unittest.TestCase):
         self.assertEqual(self.fr4.change_bit(1,6),67)
         self.assertEqual(self.fr4.change_bit(0,2),67)
 
+    def test_ones_complement(self):
+        self.fr1.ones_complement()
+        self.assertEqual(self.fr1.bits,242)
+
     def test_rotate_left(self):
         #fr1
         self.assertEqual(self.fr1.rotate_left(0),26)
@@ -114,6 +118,20 @@ class TestFileRegister(unittest.TestCase):
         self.assertEqual(self.fr2.set_all(),0b11111111)
         self.assertEqual(self.fr3.set_all(),0b11111111)
         self.assertEqual(self.fr4.set_all(),0b11111111)
+
+    def test_cycle_between_min_and_max(self):
+        self.assertEqual(self.fr1.cycle_between_min_and_max(280),24)
+        self.assertEqual(self.fr2.cycle_between_min_and_max(290),34)
+        self.assertEqual(self.fr3.cycle_between_min_and_max(300),44)
+        self.assertEqual(self.fr4.cycle_between_min_and_max(255),255)
+        self.assertEqual(self.fr4.cycle_between_min_and_max(520),8)
+
+    def test_assign_bits(self):
+        self.assertEqual(self.fr1.assign_bits(255),255)
+        self.assertEqual(self.fr2.assign_bits(280),24)
+        self.assertEqual(self.fr3.assign_bits(283),27)
+        self.assertEqual(self.fr4.assign_bits(512),0)
+        self.assertEqual(self.fr4.assign_bits(540),28)
     
 if __name__=="__main__":
     unittest.main()
