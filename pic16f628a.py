35f17dd1711eac2ed7144ecc5f56259f560dd8c6
A. Taha Baki <a.taha.baki@outlook.com>
2021-01-25 06:00:31 +0300

    Z Flag for andwf

--- pic16f628a.py
+++ pic16f628a.py
@@ -189,8 +189,10 @@ class PIC16F628A:
     def andwf(self,f,d):
         if d == 1:
             self.RAM[f].assign_bits(self.RAM[f].bits & self.Accumulator.bits)
+            self.__status_zero_flag(self.RAM[f].bits)
         elif d == 0:
             self.Accumulator.assign_bits(self.RAM[f].bits & self.Accumulator.bits)
+            self.__status_zero_flag(self.Accumulator.bits)
         self.__increase_KCS()
 
     def iorlw(self,k):
